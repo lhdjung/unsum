f1b7e91a357219510ef5f1623c81d5955cf8c3fc
Lukas Jung <jung-lukas@gmx.net>
2024-11-25 19:53:01 +0100

    rename `closure_count_initial()`

--- R/count.R
+++ R/count.R
@@ -1,7 +1,7 @@
 
 #' Count initial CLOSURE combinations
 #'
-#' @param min_scale,max_scale Integers (length 1 each). Minimum and maximum of
+#' @param scale_min,scale_max Integers (length 1 each). Minimum and maximum of
 #'   the scales to which the reported statistics refer.
 #'
 #' @return
@@ -9,13 +9,12 @@
 #'
 #' @examples
 
-# Each combination starts with two numbers i,j where min_scale ≤ i ≤ j ≤ max_scale
-# This is equivalent to choosing 2 numbers with replacement where order doesn't matter
-# The formula is: (n+1) * n / 2 where n is the range size
+# (By Claude:) Each combination starts with two numbers i,j where scale_min <= i
+# <= j <= scale_max
+# This is equivalent to choosing 2 numbers with replacement where order doesn't
+# matter The formula is: (n+1) * n / 2 where n is the range size
 
-# This make more sense on the Rust level, maybe even exported from closure-core?
-
-closure_count_combinations_initial <- function(min_scale, max_scale) {
-    range_size <- max_scale - min_scale + 1
+closure_count_initial <- function(scale_min, scale_max) {
+    range_size <- scale_max - scale_min + 1
     (range_size * (range_size + 1)) / 2
 }
