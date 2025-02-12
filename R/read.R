06077fee2fde64d019a60ceded286180b09a15cb
Lukas Jung <jung-lukas@gmx.net>
2025-02-12 21:32:32 +0100

    Update temp file reading function

--- R/read.R
+++ R/read.R
@@ -26,15 +26,28 @@ closure_read <- function(version = c("new", "old", "python")) {
 
   # Read all columns as integer:
   path %>%
-    readr::read_csv(col_types = readr::cols(.default = "i")) %>%
+    readr::read_csv(col_types = readr::cols(.default = "i"))
     # dplyr::slice(1:5000) %>%
-    add_class("closure_combine")
 }
 
 
 # # Load CLOSURE output data
 # data_rust   <- closure_read("new")
 # data_python <- closure_read("python")
+# data_r <- closure_combine(
+#   mean = "3.5",
+#   sd = "0.5",
+#   n = 52,
+#   scale_min = 1,
+#   scale_max = 5
+# )
+# data_r <- data_r$results$combination %>%
+#   tibble::as_tibble(.name_repair = "minimal") %>%
+#   t() %>%
+#   tibble::as_tibble(.name_repair = function(x) paste0("n", seq_along(x)))
 #
 # identical_except_attributes(data_rust, data_python)
 # waldo::compare(data_rust, data_python, x_arg = "rust", y_arg = "python")
+# waldo::compare(data_rust, data_r, x_arg = "rust", y_arg = "r")
+
+
