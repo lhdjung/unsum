344edca632da5cb3e9a904cdcecf09179f614849
Lukas Jung <jung-lukas@gmx.net>
2025-03-01 19:13:59 +0100

    Setup script to save package data
    * Move `closure_read()` helper from R/read.R to data-raw/closure-data.R
    * Delete read.R
    * Update `closure_read()`: "rust" option replaces outdated "old" and "new" options
    * Use together with `usethis::use_data()` to save Rust and Python results as package data

---
+++ data-raw/closure-data.R
@@ -0,0 +1,34 @@
+## code to prepare datasets containing results of other CLOSURE implementations
+## goes here
+
+closure_read <- function(version = c("rust", "python")) {
+
+  version <- rlang::arg_match(version)
+
+  # Load local file. Modify paths if needed.
+  path <- if (version == "rust") {
+    "/home/lukas/Documents/rust_projects/closure-core/parallel_results.csv"
+  } else if (version == "python") {
+    "/home/lukas/Documents/python_projects/CLOSURE-Python/parallel_results.csv"
+  } else {
+    stop("Invalid `version` argument")
+  }
+
+  # Read all columns as integer:
+  readr::read_csv(path, col_types = readr::cols(.default = "i"))
+}
+
+
+# Load CLOSURE output data of Rust and Python implementations
+.data_rust   <- closure_read("rust")
+.data_python <- closure_read("python")
+
+# Save these datasets to disk. They will be internally available to the package,
+# so they can be used by unit tests. However, they are not exported -- this is
+# why their names start with dots.
+usethis::use_data(
+  .data_rust,
+  .data_python,
+  internal = TRUE
+)
+
