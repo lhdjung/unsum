6d9ebd4c4316c775bf6b8670146093eaffde35da
Lukas Jung <jung-lukas@gmx.net>
2025-05-25 14:59:17 +0200

    Complete `closure_combine()` docs

--- R/combine.R
+++ R/combine.R
@@ -43,7 +43,7 @@
 #'   - **`inputs`**: Arguments to this function.
 #'   - **`metrics`**:
 #'     - `combos_initial`: integer. The basis for computing CLOSURE results,
-#'   based on scale range only.
+#'   based on scale range only. See [`closure_count_initial()`].
 #'     - `combos_all`: integer. Number of all combinations. Equal to the number
 #'   of rows in `results`.
 #'     - `values_all`: integer. Number of all individual values found. Equal to
@@ -55,6 +55,7 @@
 #'   - **`frequency`**:
 #'     - `value`: integer. Scale values derived from `scale_min` and
 #'   `scale_max`.
+#'     - `f_average`: Count of scale values in the mean `results` combination.
 #'     - `f_absolute`: integer. Count of individual scale values found in the
 #'   `results` combinations.
 #'     - `f_relative`: double. Values' share of total values found.
