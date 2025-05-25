97ebf2cd1dc8eba9d6fd9fcaa3a623e09fe00f26
Lukas Jung <jung-lukas@gmx.net>
2025-05-25 22:56:02 +0200

    Add more tests for `closure_combine()`

--- tests/testthat/test-combine.R
+++ tests/testthat/test-combine.R
@@ -30,3 +30,37 @@ test_that("All implementations return identical results (after sorting columns)"
 # waldo::compare(.data_rust,   .data_python, x_arg = "rust",   y_arg = "python")
 # waldo::compare(.data_rust,   data_r,       x_arg = "rust",   y_arg = "r")
 # waldo::compare(.data_python, data_r,       x_arg = "python", y_arg = "r")
+
+
+f_absolute_centered <- closure_combine(
+  mean = "4.0",
+  sd = "1.0",
+  n = 50,
+  scale_min = 1,
+  scale_max = 7
+)$frequency$f_absolute
+
+f_absolute_skewed_left <- closure_combine(
+  mean = "2.5",
+  sd = "1.7",
+  n = 50,
+  scale_min = 1,
+  scale_max = 7
+)$frequency$f_absolute
+
+f_absolute_skewed_right <- closure_combine(
+  mean = "5.3",
+  sd = "1.7",
+  n = 50,
+  scale_min = 1,
+  scale_max = 7
+)$frequency$f_absolute
+
+
+test_that("absolute frequencies are correct", {
+  f_absolute_centered     |> expect_equal(c(6575L, 17570L, 65388L, 209734L, 65388L, 17570L, 6575L))
+  f_absolute_skewed_left  |> expect_equal(c(571399L, 341334L, 194430L, 147830L, 107079L, 69040L, 44938L))
+  f_absolute_skewed_right |> expect_equal(c(80279L, 123068L, 191619L, 269477L, 357529L, 626871L, 708557L))
+})
+
+
