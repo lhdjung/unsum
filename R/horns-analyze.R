ab675cfee081ff12c5e8a4787f76e9191bb94b46
Lukas Jung <jung-lukas@gmx.net>
2025-06-15 20:04:41 +0200

    Unify docs of `closure_horns_*()` plot functions
    * Also, remove one check in `closure_horns_analyze()` that was based on an error
    * Change `bar_alpha` default in `closure_horns_histogram()` to `0.75`
    * Move the two functions into a new file

--- R/horns-analyze.R
+++ R/horns-analyze.R
@@ -2,47 +2,51 @@
 #'
 #' @description Following up on [`closure_generate()`], you can call
 #'   `closure_horns_analyze()` to compute the horns index for each individual
-#'   sample and compute summary statistics on the distribution of these indices.
-#'   See [`horns()`] for the metric itself.
+#'   sample and calculate summary statistics on the distribution of these
+#'   indices. See [`horns()`] for the metric itself.
 #'
-#'   This adds more detail to the `"horns"` and `"horns_uniform"` columns in the
-#'   output of `closure_generate()`, where `"horns"` is the overall mean of the
-#'   per-sample indices found here.
+#'   See [horns_plot] for follow-up visualizations.
 #'
-#'   `closure_horns_histogram()` draws a quick barplot to reveal the
-#'   distribution of horns values. The scale is fixed between 0 and 1.
+#' @param data List returned by [`closure_generate()`].
 #'
-#' @param data For `closure_horns_analyze()`, a list returned by
-#'   `closure_generate()`. For `closure_horns_histogram()`, a list returned by
-#'   `closure_horns_analyze()`.
-#' @param bar_alpha Numeric (length 1). Opacity of the bars. Default is `0.8`.
-#' @param bar_binwidth Width of the bins that divide up the x-axis, passed on to
-#'   [`ggplot2::geom_histogram()`]. Default is `0.0025`.
-#' @inheritParams closure_plot_bar
+#' @details The results of `closure_generate()` already include a `"horns"`
+#'   metric that is computed from the frequencies of scale values in the mean
+#'   sample. This is different from the `"mean"` metric found here, which is the
+#'   mean of the horns indices of all samples. These two values can differ
+#'   because the horns index is based on squared deviations, which can result in
+#'   non-linearities.
 #'
-#' @details The `"mad"` column overrides a default of `stats::mad()`: adjusting
-#'   the result via multiplication by a constant (about 1.48). This assumes a
-#'   normal distribution, which generally does not seem to be the case with
-#'   horns index values. Here, the constant is set to 1.
+#'   The `"mad"` column is created by [`stats::mad()`], but it does not rely on
+#'   this function's default of adjusting the result via multiplication by a
+#'   constant (about 1.48). This assumes a normal distribution, which is not
+#'   generally the case with horns index values. Here, the constant is set to
+#'   `1`.
 #'
-#' @return `closure_horns_analyze()` returns a named list of two tibbles (data
-#'   frames):
+#' @return Named list of five tibbles (data frames):
+#'   - **closure_generate_inputs**: Adopted from the input.
 #'   - **horns_metrics**: Summary statistics of the distribution of horns index
 #'   values:
-#'     - `mean`, `uniform`: same as `horns` and `horns_uniform` from
-#'   `closure_generate()`'s output.
+#'     - `mean`, `uniform`: arithmetic mean.
+#'     - `uniform`: horns value of a hypothetical uniform distribution. Equal to
+#'   `horns_uniform` from the results of `closure_generate()`.
 #'     - `sd`: double. Standard deviation.
 #'     - `cv`: double. Coefficient of variation, i.e., `sd / mean`.
-#'     - `mad`: double. Median absolute deviation; see [`stats::mad()`].
+#'     - `mad`: double. Median absolute deviation; see [`stats::mad()`] but also
+#'   details here.
 #'     - `min`, `median`, `max`: double. Minimum, median, and maximum horns
 #'   index.
 #'     - `range`: double. Equal to `max - min`.
+#'   - **frequency_horns_min**, **frequency_horns_max**: Frequency tables of the
+#'   samples with the minimum or maximum horns values. They are structured like
+#'   the `frequency` table in the output of `closure_generate()`. However, note
+#'   that the `f_absolute` column here can include values from multiple samples,
+#'   so it is better to focus on `f_average` or `f_relative`. These two refer to
+#'   the average sample from the subset of samples with the minimum or maximum
+#'   horns value.
 #'   - **horns_results**:
 #'     - `id`: integer. Uniquely identifies each horns index, just like their
 #'   corresponding samples in `closure_generate()`.
 #'     - `horns`: double. Horns index for each individual sample.
-#'
-#'   `closure_horns_histogram()` returns a ggplot object.
 #'
 #' @include horns.R plot.R utils.R
 #'
@@ -57,10 +61,7 @@
 #'   scale_max = 5
 #' )
 #'
-#' data_horns <- closure_horns_analyze(data)
-#' data_horns
-#'
-#' closure_horns_histogram(data_horns)
+#' closure_horns_analyze(data)
 
 closure_horns_analyze <- function(data) {
   check_closure_generate(data)
@@ -90,18 +91,6 @@ closure_horns_analyze <- function(data) {
   }
 
   mean <- mean(horns_values)
-
-  if (!near(mean, data$metrics$horns, tol = 0.01)) {
-    cli::cli_abort(
-      message = c(
-        "Mean not the same as the \"horns\" column \
-        in the output of `closure_generate()`.",
-        "x" = "Mean: {mean}",
-        "x" = "\"horns\" column: {data$metrics$horns}"
-      )
-    )
-  }
-
   sd <- sd(horns_values)
   min <- min(horns_values)
   max <- max(horns_values)
@@ -172,95 +161,3 @@ closure_horns_analyze <- function(data) {
   )
 }
 
-
-# Arguments for this function are generated below the definition
-closure_horns_min_max_bar <- function() {
-
-  check_closure_horns_analyze(data)
-  check_length(facet_labels, 2L)
-
-  min_max <- rlang::arg_match(min_max)
-
-  names_min_max <- c(
-    "frequency_horns_min",
-    "frequency_horns_max"
-  )
-
-  plot_frequency_bar(
-    data = data,
-    frequency = frequency,
-    samples = samples,
-    name_frequency_table = switch(
-      min_max,
-      "both" = names_min_max,
-      "min" = names_min_max[1L],
-      "max" = names_min_max[2L]
-    ),
-    facet_labels = facet_labels,
-    bar_alpha = bar_alpha,
-    bar_color = bar_color,
-    show_text = show_text,
-    text_color = text_color,
-    text_size = text_size,
-    text_offset = text_offset,
-    mark_thousand = mark_thousand,
-    mark_decimal = mark_decimal
-  )
-}
-
-formals(closure_horns_min_max_bar) <- plot_frequency_bar |>
-  formals() |>
-  formals_change_defaults(
-    bar_color = "#5D3FD3"
-  ) |>
-  formals_change_defaults(
-    facet_labels = c("Minimal variability", "Maximal variability")
-  ) |>
-  formals_add(
-    min_max = c("both", "min", "max"),
-    .after = 1L
-  ) |>
-  formals_remove("name_frequency_table")
-
-
-#' @rdname closure_horns_analyze
-#' @export
-
-closure_horns_histogram <- function(
-  data,
-  bar_alpha = 0.8,
-  bar_color = "#5D3FD3",
-  bar_binwidth = 0.0025,
-  text_size = 12
-) {
-
-  check_closure_horns_analyze(data)
-
-  min <- data$horns_metrics$min
-  max <- data$horns_metrics$max
-
-  data <- data$horns_results
-
-  # Construct the plot
-  ggplot2::ggplot(data) +
-    ggplot2::geom_histogram(
-      ggplot2::aes(x = horns),
-      alpha = bar_alpha,
-      fill = bar_color,
-      binwidth = bar_binwidth
-    ) +
-    ggplot2::scale_x_continuous(
-      limits = c(0, 1),
-      oob = function(x, limits) x
-    ) +
-    ggplot2::labs(
-      x = "Horns index (h)",
-      y = "Count in all horns index values"
-    ) +
-    ggplot2::theme_minimal(base_size = text_size) +
-    ggplot2::theme(
-      panel.grid.major.x = ggplot2::element_blank(),
-      panel.grid.minor.x = ggplot2::element_blank(),
-      panel.grid.minor.y = ggplot2::element_blank()
-    )
-}
---
+++ R/horns-plot.R
@@ -0,0 +1,143 @@
+#' Visualize horns values distribution
+#'
+#' @description Two functions that follow up on [`closure_horns_analyze()`]:
+#' - `closure_horns_min_max_bar()` draws barplots of the mean samples from among
+#'   those with the minimum or maximum horns index. It displays the typical
+#'   sample with the least or most amount of variability from among all CLOSURE
+#'   samples.
+#' - `closure_horns_histogram()` draws a quick barplot of the distribution of
+#'   horns values. The scale is fixed between 0 and 1, so it is aligned with the
+#'   range of the horns index. This reveals the big picture, putting any
+#'   variability among horns values into perspective.
+#'
+#' @param data List returned by [`closure_horns_analyze()`].
+#' @param min_max String (length 1). Only in `closure_horns_min_max_bar()`.
+#'   Which plot(s) to show? Options are `"both"` (the default), `"min"`, and
+#'   `"max"`.
+#' @param facet_labels String (length 2). Only in `closure_horns_min_max_bar()`.
+#'   Labels of the two individual plots. Default is `c("Minimal variability",
+#'   "Maximal variability")`.
+#' @param bar_binwidth Numeric (length 1). Only in `closure_horns_histogram()`.
+#'   Width of the bins that divide up the x-axis, passed on to
+#'   [`ggplot2::geom_histogram()`]. Default is `0.0025`.
+#' @inheritParams closure_plot_bar
+#'
+#' @name horns_plot
+#'
+#' @examples
+#' # Preparation (1 / 2): run CLOSURE
+#' data <- closure_generate(
+#'   mean = "2.9",
+#'   sd = "0.5",
+#'   n = 70,
+#'   scale_min = 1,
+#'   scale_max = 5
+#' )
+#'
+#' # Preparation (2 / 2): analyze sample-wise horns values
+#' data_horns <- closure_horns_analyze(data)
+#'
+#' # Even with minimal and maximal variability,
+#' # the results are almost the same:
+#' closure_horns_min_max_bar(data_horns)
+#'
+#' # They cluster in a narrow slice of the 0-1 range
+#' # of the horns index:
+#' closure_horns_histogram(data_horns)
+
+
+#' @rdname horns_plot
+#' @export
+
+# Arguments for this function are generated below the definition
+closure_horns_min_max_bar <- function() {
+
+  check_closure_horns_analyze(data)
+  check_length(facet_labels, 2L)
+
+  min_max <- rlang::arg_match(min_max)
+
+  names_min_max <- c(
+    "frequency_horns_min",
+    "frequency_horns_max"
+  )
+
+  plot_frequency_bar(
+    data = data,
+    frequency = frequency,
+    samples = samples,
+    name_frequency_table = switch(
+      min_max,
+      "both" = names_min_max,
+      "min" = names_min_max[1L],
+      "max" = names_min_max[2L]
+    ),
+    facet_labels = facet_labels,
+    bar_alpha = bar_alpha,
+    bar_color = bar_color,
+    show_text = show_text,
+    text_color = text_color,
+    text_size = text_size,
+    text_offset = text_offset,
+    mark_thousand = mark_thousand,
+    mark_decimal = mark_decimal
+  )
+}
+
+formals(closure_horns_min_max_bar) <- plot_frequency_bar |>
+  formals() |>
+  formals_change_defaults(
+    bar_color = "#5D3FD3"
+  ) |>
+  formals_change_defaults(
+    facet_labels = c("Minimal variability", "Maximal variability")
+  ) |>
+  formals_add(
+    min_max = c("both", "min", "max"),
+    .after = 1L
+  ) |>
+  formals_remove("name_frequency_table")
+
+
+#' @rdname horns_plot
+#' @export
+
+closure_horns_histogram <- function(
+    data,
+    bar_alpha = 0.75,
+    bar_color = "#5D3FD3",
+    bar_binwidth = 0.0025,
+    text_size = 12
+) {
+
+  check_closure_horns_analyze(data)
+
+  min <- data$horns_metrics$min
+  max <- data$horns_metrics$max
+
+  data <- data$horns_results
+
+  # Construct the plot
+  ggplot2::ggplot(data) +
+    ggplot2::geom_histogram(
+      ggplot2::aes(x = horns),
+      alpha = bar_alpha,
+      fill = bar_color,
+      binwidth = bar_binwidth
+    ) +
+    ggplot2::scale_x_continuous(
+      limits = c(0, 1),
+      oob = function(x, limits) x
+    ) +
+    ggplot2::labs(
+      x = "Horns index (h)",
+      y = "Count in all horns index values"
+    ) +
+    ggplot2::theme_minimal(base_size = text_size) +
+    ggplot2::theme(
+      panel.grid.major.x = ggplot2::element_blank(),
+      panel.grid.minor.x = ggplot2::element_blank(),
+      panel.grid.minor.y = ggplot2::element_blank()
+    )
+}
+
