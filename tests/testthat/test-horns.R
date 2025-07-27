07536796aaf9970cbc56d7d32ee3b1f0628233fc
Lukas Jung <jung-lukas@gmx.net>
2025-07-27 12:03:37 +0200

    Add more tests

--- tests/testthat/test-horns.R
+++ tests/testthat/test-horns.R
@@ -1,8 +1,12 @@
 
 test_that("`horns()` works correctly in the 1-5 case", {
-  c(300, 0, 0, 0, 0)    |> horns(1, 5) |> expect_equal(0)
-  c(150, 0, 0, 0, 150)  |> horns(1, 5) |> expect_equal(1)
-  c(60, 60, 60, 60, 60) |> horns(1, 5) |> expect_equal(0.5)
+  c(300, 0, 0, 0, 0)      |> horns(1, 5) |> expect_equal(0)
+  c(150, 0, 0, 0, 150)    |> horns(1, 5) |> expect_equal(1)
+  c(100, 0, 0, 0, 200)    |> horns(1, 5) |> expect_equal(0.8888889)
+  c(60, 60, 60, 60, 60)   |> horns(1, 5) |> expect_equal(0.5)
+  c(200, 50, 30, 20, 0)   |> horns(1, 5) |> expect_equal(0.2113889, tolerance = 1e-6)
+  c(150, 100, 50, 0, 0)   |> horns(1, 5) |> expect_equal(0.1388889, tolerance = 1e-6)
+  c(100, 40, 20, 40, 100) |> horns(1, 5) |> expect_equal(0.7333333, tolerance = 1e-6)
 })
 
 
