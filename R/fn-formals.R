43f396b13422003559bf865540a7792ae33aa404
Lukas Jung <jung-lukas@gmx.net>
2025-12-30 18:59:57 +0100

    More systematic argument modifiers
    * Add some `formals_*()` functions (not currently used)
    * Group all of those functions into four categories
    * Type safety: all constructors and modifiers now return pairlists
    * More robust removal strategy in zzz.R

--- R/fn-formals.R
+++ R/fn-formals.R
@@ -1,5 +1,72 @@
+# About this file ---------------------------------------------------------
+
+# The functions in this file create, modify, or test a function's list of
+# arguments. They are grouped into these sections:
+
+# -- "Constructors" create a list of arguments from scratch. The names of these
+# functions all start on `formals_new_`.
+# -- "Modifiers" change an existing list of arguments. Example usage:
+# `your_function |> formals() |> formals_add("mode", .after = "data")`
+# -- "Predicates" run checks on a list of arguments and return logical vectors.
+# -- "Helpers" are only needed by the other functions.
+
+# If you use them as build helpers and don't need them in the final binary, you
+# could run `rm(list = ls(pattern = "^formals_"))` in a zzz.R file. This will
+# remove all these functions before they can be included in the package binary.
+
+# Dependencies: rlang and cli only, and cli is only used to throw errors. So
+# they could be rewritten to remove the cli dependency, but this would make the
+# errors less helpful. Some of the functions rely on other functions here.
+
+# Constructors ------------------------------------------------------------
+
+# Alternative to `formals()` with the same arguments but these differences:
+# -- It doesn't return `NULL` for primitives. Instead, it returns the list of
+# "pseudo-arguments" that the primitive function would have if it was a closure.
+# E.g., `args()` shows these pseudo-arguments but doesn't directly return them.
+# -- It throws an error if `fun` is not a function.
+formals_new_pseudo <- function(fun, envir = parent.frame(2)) {
+  if (is.primitive(fun)) {
+    formals(args(fun), envir)
+  } else if (is.function(fun)) {
+    formals(fun, envir)
+  } else {
+    cli::cli_abort(c(
+      "`fun` must be a function.",
+      "x" = "Its actual type is: {typeof(fun)}"
+    ))
+  }
+}
+
+
+# Create a pairlist of arguments without defaults. Similar to, and based on,
+# `rlang::pairlist2()` when used like `pairlist2(a = , b = )`. However, the
+# current function is more flexible because it can even create such lists
+# programmatically from a string vector, without hard-coding the arguments.
+formals_new_without_defaults <- function(...) {
+  names_all <- c(...)
+
+  if (!is.character(names_all) || !all(rlang::names2(names_all) == "")) {
+    cli::cli_abort(
+      "Arguments of `formals_new_without_defaults()` must be unnamed strings."
+    )
+  }
+
+  # Prepare a makeshift list -- not yet a pairlist -- of arguments
+  out <- vector("list", length(names_all))
+  names(out) <- names_all
+
+  # Transform the list into a pairlist and remove its `NULL` defaults
+  formals_remove_defaults(out, names_all)
+}
+
+
+# Modifiers ---------------------------------------------------------------
+
 # Replace the default of an existing argument by a new default
 formals_change_defaults <- function(formals_fn, ...) {
+  check_pairlist(formals_fn)
+
   new_defaults <- list(...)
 
   if (length(new_defaults) == 0) {
@@ -20,7 +87,8 @@ formals_change_defaults <- function(formals_fn, ...) {
   }
 
   formals_fn[names(formals_fn) %in% names(new_defaults)] <- new_defaults
-  formals_fn
+
+  as.pairlist(formals_fn)
 }
 
 
@@ -28,6 +96,8 @@ formals_change_defaults <- function(formals_fn, ...) {
 # themselves. Use by passing strings (through the dots) that are the names of
 # the arguments you want to free from their defaults.
 formals_remove_defaults <- function(formals_fn, ...) {
+  check_pairlist(formals_fn)
+
   targets_all <- c(...)
 
   names_all <- names(formals_fn)
@@ -59,7 +129,15 @@ formals_remove_defaults <- function(formals_fn, ...) {
     )))
   }
 
-  formals_fn
+  # Arguments are stored in pairlists, not regular lists
+  as.pairlist(formals_fn)
+}
+
+
+# Simple wrapper that removes all defaults from a list of arguments
+formals_remove_defaults_all <- function(formals_fn) {
+  check_pairlist(formals_fn)
+  formals_remove_defaults(formals_fn, names(formals_fn))
 }
 
 
@@ -69,6 +147,8 @@ formals_remove_defaults <- function(formals_fn, ...) {
 # `list_of_formals |> formals_add(a = 5, "b")` will add `a` as an argument with
 # default `5`, and `b` as an argument without a default; in this order.
 formals_add <- function(formals_fn, ..., .before = NULL, .after = NULL) {
+  check_pairlist(formals_fn)
+
   new_args_all <- list(...)
 
   # Unlike `base::names()`, `rlang::names2()` returns `""` for unnamed elements
@@ -145,7 +225,7 @@ formals_add <- function(formals_fn, ..., .before = NULL, .after = NULL) {
     # Therefore, `NULL` is inserted instead if there is no second part. This
     # problem can't occur on the lower end because the index only ever
     # increments, and because R can inherently handle the lower end better.
-    formals_fn <- c(
+    formals_fn <- as.pairlist(c(
       formals_fn[seq_len(neighbor_low)],
       new_arg,
 
@@ -154,7 +234,7 @@ formals_add <- function(formals_fn, ..., .before = NULL, .after = NULL) {
       } else {
         formals_fn[neighbor_high:length(formals_fn)]
       }
-    )
+    ))
 
     # Remove temporary dummy default
     if (has_no_default) {
@@ -165,12 +245,14 @@ formals_add <- function(formals_fn, ..., .before = NULL, .after = NULL) {
     index <- index + 1
   }
 
-  formals_fn
+  as.pairlist(formals_fn)
 }
 
 
 # Remove selected arguments from the list of a function's arguments
 formals_remove <- function(formals_fn, ...) {
+  check_pairlist(formals_fn)
+
   targets_all <- c(...)
   offenders <- targets_all[!targets_all %in% names(formals_fn)]
 
@@ -185,5 +267,45 @@ formals_remove <- function(formals_fn, ...) {
   }
 
   should_be_removed <- names(formals_fn) %in% targets_all
-  formals_fn[!should_be_removed]
+
+  as.pairlist(formals_fn[!should_be_removed])
+}
+
+
+# Predicates --------------------------------------------------------------
+
+# For each argument in a list of arguments, this says `TRUE` if the argument has
+# a default, and `FALSE` if it does not
+formals_have_defaults <- function(formals_fn) {
+  check_pairlist(formals_fn)
+
+  out <- logical(length(formals_fn))
+
+  for (i in seq_along(formals_fn)) {
+    current_arg <- formals_fn[[i]]
+
+    if (!missing(current_arg)) {
+      out[i] <- TRUE
+    }
+  }
+
+  out
+}
+
+
+# Helpers -----------------------------------------------------------------
+
+# Helper that throws an error if its argument is not a pairlist, i.e., the
+# special type of object that a function's list of arguments consist of.
+check_pairlist <- function(x) {
+  if (typeof(x) != "pairlist") {
+    cli::cli_abort(
+      c(
+        "`formals_fn` must be the list of a function's arguments.",
+        "x" = "Its actual type is: {typeof(x)}",
+        "i" = "Get a function's arguments using `formals(your_function)`."
+      ),
+      call = rlang::caller_env()
+    )
+  }
 }
--- R/zzz.R
+++ R/zzz.R
@@ -1,16 +1,17 @@
 # Cleanup script to remove build helpers that are only needed when the package
 # is built, not when it runs on the user's machine. Removing these objects
 # prevents them from inflating the binary, i.e., the final package file.
+
+# Argument modifiers: change the list of a function's arguments
+rm(list = ls(pattern = "^formals_"))
+
+last <- NULL
+
 rm(
   # Function factories: build parameterized wrappers around more basic functions
   new_generator_mean_sd_n,
   new_plot_fn_bar,
   new_plot_fn_horns_frequency,
-  # Argument modifiers: change the list of a function's formal arguments
-  formals_add,
-  formals_remove,
-  formals_remove_defaults,
-  formals_change_defaults,
-  # This is a list, but all other objects are functions
-  formals_final
+  # Placeholder for possible future extensions
+  last
 )
