cfe42236136cc8b898c06ea54d17b16ac1b6c172
Lukas Jung <jung-lukas@gmx.net>
2026-02-16 16:42:21 +0100

    Enable repeated read-write tests

--- tests/testthat/test-read-write.R
+++ tests/testthat/test-read-write.R
@@ -1,4 +1,7 @@
-temp_folder <- tempdir()
+# Enable the test file to be run multiple times per session
+temp_folder <- file.path(tempdir(), "unsum-test-read-write")
+dir.create(temp_folder, showWarnings = FALSE)
+withr::defer(unlink(temp_folder, recursive = TRUE), teardown_env())
 
 path_closure <- closure_generate(
   mean = "3.5",
@@ -16,5 +19,3 @@ test_that("`closure_read()` works", {
     check_generator_output("CLOSURE") |>
     expect_no_error()
 })
-
-unlink("path_closure", recursive = TRUE)
