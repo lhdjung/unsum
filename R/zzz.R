7aaf125496ac721e05ae4d9d00437e9beb2ea9f4
Lukas Jung <jung-lukas@gmx.net>
2026-02-24 15:03:43 +0100

    Build-ignore doc-helpers.R
    * This file only contains helpers needed in between sourcing and building
    * Clarify in zzz.R

--- R/zzz.R
+++ R/zzz.R
@@ -2,6 +2,9 @@
 # is built, not when it runs on the user's machine. Removing these objects
 # prevents them from inflating the binary, i.e., the final package file.
 
+# Use .Rbuildignore instead for files like doc-helpers.R that are needed between
+# sourcing the R/ files and actually building the package!
+
 # Argument modifiers: change the list of a function's arguments
 rm(list = ls(pattern = "^formals_"))
 
@@ -12,10 +15,12 @@ rm(
   new_generator_mean_sd_n,
   new_plot_fn_bar,
   new_plot_fn_horns_frequency,
+
   # Build mode setters
   use_debug,
   use_release,
   switch_build_mode,
+
   # Placeholder for possible future extensions
   last
 )
