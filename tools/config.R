a4b0e3456bf5a397c65674af2304dceeb893af6e
Lukas Jung <jung-lukas@gmx.net>
2026-03-01 18:12:55 +0100

    Extendr upkeep

--- tools/config.R
+++ tools/config.R
@@ -12,11 +12,8 @@ env_not_cran <- Sys.getenv("NOT_CRAN")
 vendor_exists <- file.exists("src/rust/vendor.tar.xz")
 
 is_not_cran <- env_not_cran != ""
-is_debug <- FALSE
+is_debug <- env_debug != ""
 
-if (!is_not_cran) {
-  is_debug <- FALSE
-}
 
 if (is_debug) {
   # if we have DEBUG then we set not cran to true
